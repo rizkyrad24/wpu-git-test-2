107cbf13922b7ee5efa4a6059ec7461a2f166427
rizkyrad24 <rizkyasaprofessional@gmail.com>
2022-10-02 16:59:53 +0700

    menampahkan paragraf

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam facere perspiciatis fugiat quibusdam hic molestias atque tempora eius sapiente minima doloribus qui, aut quam. Vero nisi eius nihil harum nemo!</p>
 </body>
 </html>
